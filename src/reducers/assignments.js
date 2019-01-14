51afc55cac6e5694163a99eaba2b49b5337eb2d7
Andrew Lincoln <andrewjlincoln@gmail.com>
2019-01-14 14:35:23 -0800

    adds create assignment page

---
+++ src/reducers/assignments.js
@@ -0,0 +1,17 @@
+// import {GET_POSTS} from '../actions/posts'
+// import {FILTER_POSTS} from '../actions/posts'
+
+
+
+function assignments(state = [], action) {
+    
+    switch(action.type) {
+    
+
+        default: 
+            return state 
+    }
+}
+
+
+export default assignments
