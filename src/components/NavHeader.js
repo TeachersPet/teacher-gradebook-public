c31eea262acc65b48182d74e7e887447aa70dda7
Kevin Parine <kparine314@gmail.com>
2019-01-14 09:59:13 -0800

    Add Sass

---
+++ src/components/NavHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+function NavHeader() {
+
+return(
+  <nav className="navbar navbar-dark">
+      <a className="navbar-brand"><strong><span aria-label='cart'>📚 </span> Teachers Pet. </strong></a>
+  </nav>
+  )
+}
+export default NavHeader
